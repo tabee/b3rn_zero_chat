85a4ed86f4d68e5f7b49b80644b4bca70c5c90e9
tabee <bee.mario@gmail.com>
2023-06-09 19:11:50 +0000

    codespaces

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,6 +1,6 @@
 """tool's"""
 from .sitemaper import make_sitemap
 from .webextractor import get_documents_from_sitemap
-from .vectorstore import add_stuff_to_store, load_vectorstore
+from .vectorstore import add_stuff_to_store, load_vectorstore, get_embeddings
 
 print("all tools initialized.")
